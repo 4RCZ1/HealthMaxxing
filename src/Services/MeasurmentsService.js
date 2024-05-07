4804764c5cb5fe50d5d8769810114a61bb3dfa9d
Jager72 <49473040+Jager72@users.noreply.github.com>
2024-05-07 21:40:09 +0200

    feat: update to measurementsService

--- src/Services/MeasurmentsService.js
+++ src/Services/MeasurmentsService.js
@@ -1,18 +1,30 @@
 class Measurement {
-    constructor(name, date, value, userId) {
+    constructor(name, date, value, userId, measurementType) {
         this.name = name;
         this.date = date;
         this.value = value;
         this.userId = userId;
+        this.measurementType = measurementType;
     }
 }
+
+measurementType
+
+class measurementType {
+    constructor(name, date, value, userId, measurementType) {
+        this.name = name;
+        this.date = date;
+        this.value = value;
+        this.userId = userId;
+        this.measurementType = measurementType;
+    }
+}
+
 
 class MeasurementsService {
     constructor(db) {
         this.db = db;
-    }
-
-    
+    }    
 
     async getAllMeasurements(userId, token) {
         try {
@@ -34,7 +46,8 @@ class MeasurementsService {
 
     async updateMeasurement(id, update, token) {
         try {
-            const decodedToken = jwtService.verifyToken(token, id);
+            expectedUserId = update.userId
+            const decodedToken = jwtService.verifyToken(token, expectedUserId);
             console.log(decodedToken);
         } catch (error) {
             console.error(error.message);
@@ -48,8 +61,9 @@ class MeasurementsService {
     }
 
     async deleteMeasurement(id, token) {
+        const measurement = this.db.Measurement.findByPk(id);
         try {
-            const decodedToken = jwtService.verifyToken(token, id);
+            const decodedToken = jwtService.verifyToken(token, measurement.userId);
             console.log(decodedToken);
         } catch (error) {
             console.error(error.message);
@@ -61,6 +75,57 @@ class MeasurementsService {
             }
         });
     }
+
+    async createMeasurementType(measurementType, token) {
+        try {
+            expectedUserId = measurementType.userId
+            const decodedToken = jwtService.verifyToken(token, expectedUserId);
+            console.log(decodedToken);
+        } catch (error) {
+            console.error(error.message);
+        }
+
+        return this.db.MeasurementType.create(measurementType);
+    }
+
+    async deleteMeasurementType(id, token) {
+        try {
+            const decodedToken = jwtService.verifyToken(token, id);
+            console.log(decodedToken);
+        } catch (error) {
+            console.error(error.message);
+        }
+
+        return this.db.MeasurementType.destroy({
+            where: {
+                id: { [this.db.Op.eq]: id }
+            }
+        });
+    }
+
+    async getMeasurement(id, token) {
+
+        const measurement = this.db.Measurement.findByPk(id);
+
+        try {
+            const expectedUserId = measurement.userId
+            const decodedToken = jwtService.verifyToken(token, expectedUserId);
+            console.log(decodedToken);
+        } catch (error) {
+            console.error(error.message);
+        }
+
+        const measurementType = this.db.MeasurementType.findOne({
+            where: {
+                name: { [this.db.Op.eq]: measurement.measurementType }
+            }
+        });
+
+        return { measurement, measurementType };
+    }
+
+
+
 
 }
 

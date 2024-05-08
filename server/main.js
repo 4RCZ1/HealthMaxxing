ef4dce65e31e4993a17ae188a72343a6ad5c7fd8
Jager72 <49473040+Jager72@users.noreply.github.com>
2024-05-08 05:47:14 +0200

    feat: Added jwt

--- server/main.js
+++ server/main.js
@@ -6,7 +6,7 @@ app.get('/', (req, res) => res.send('Hello World!'));
 const DrugsService = require('./Services/DrugsServices');
 const JWTService = require('./Services/JwtService');
 const DrugsServices = new DrugsService();
-
+const JWTServices = new JWTService('100pa');
 const port = 3001;
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
@@ -18,6 +18,13 @@ app.get('/getUserPrescriptions', async (req, res) => {
   const userPrescriptions = await DrugsServices.getUserPrescriptions(token);
   res.send(userPrescriptions);
 });
+
+
+app.post('/generateToken', async (req, res) => {
+  const token = await JWTServices.generateToken(1);
+  res.send({ token });
+});
+
 
 
 

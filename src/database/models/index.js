083dbbdfcd9e21a3c09d46e380072fc05973e7b1
s485936 <maralc@st.amu.edu.pl>
2024-05-08 03:15:48 +0200

    feat: added missing files, and new UserService

---
+++ src/database/models/index.js
@@ -0,0 +1,17 @@
+import User from './User.js';
+import Drug from './Drug.js';
+import DrugPrescription from './DrugPrescription.js';
+import DrugTake from './DrugTake.js';
+import Measurement from "./Measurement";
+import MeasurementTake from "./MeasurementTake";
+import MeasurementPrescription from "./MeasurementPrescription";
+
+export {
+  User,
+  Drug,
+  DrugPrescription,
+  DrugTake,
+  Measurement,
+  MeasurementTake,
+  MeasurementPrescription
+}
---
+++ tests/integration/drugsService.test.js
@@ -0,0 +1,93 @@
+import DrugsServices from "../../src/Services/DrugsServices";
+import jWTService from "../../src/Services/JwtService";
+import sequelize from "../../src/database/sequelize";
+import {User, DrugPrescription} from "../../src/database/models";
+
+let userId;
+let doctorId;
+
+describe("DrugsServices", () => {
+  beforeAll(async () => {
+    await sequelize.sync({force: true});
+
+    userId = (await User.create({
+      name: "Test User"
+    })).id
+
+    doctorId = (await User.create({
+      name: "Test Doctor",
+      role: "doctor"
+    })).id
+  });
+
+  it("should add a new drug prescription as a doctor", async () => {
+    const drug = {
+      name: "Paracetamol",
+      frequency: "1-0-1",
+      dose: 500,
+      notes: "Take with water",
+      userId: userId
+    };
+    const JWTService = new jWTService("100pa");
+    const token = JWTService.generateToken(doctorId);
+    const drugsServices = new DrugsServices();
+    const response = await drugsServices.addDrugPrescription(drug, token);
+
+    expect(response.error).toBeUndefined();
+
+    const drugPrescription = await DrugPrescription.findAll({
+      where: {
+        userId: userId,
+        drugId: response.drugId
+      },
+      raw: true
+    });
+
+    console.log(drugPrescription)
+    expect(drugPrescription).not.toBeNull();
+  });
+  it("should add a new drug prescription to a drug that already exists", async () => {
+    const drug = {
+      name: "Paracetamol",
+      frequency: "1-0-1",
+      dose: 500,
+      notes: "Take with water",
+      userId: doctorId
+    };
+    const JWTService = new jWTService("100pa");
+    const token = JWTService.generateToken(doctorId);
+    const drugsServices = new DrugsServices();
+    const response = await drugsServices.addDrugPrescription(drug, token);
+
+    expect(response.error).toBeUndefined();
+
+    const drugPrescription = await DrugPrescription.findAll({
+      where: {
+        userId: doctorId,
+        drugId: response.drugId
+      },
+      raw: true
+    });
+
+    expect(drugPrescription).not.toBeNull();
+  });
+  it("should get user prescriptions, take a drug and then see it in the history", async () => {
+    const jwtService = new jWTService("100pa");
+    const userToken = jwtService.generateToken(userId);
+    const drugsServices = new DrugsServices();
+    const prescriptions = await drugsServices.getUserPrescriptions(userToken);
+    const firstPrescriptionId = prescriptions[0].id;
+    const takeDrugResponse = await drugsServices.takeDrug(firstPrescriptionId, userToken);
+    expect(takeDrugResponse.error).toBeUndefined();
+    const takeDrugHistory = await drugsServices.getDrugTakeHistory(userToken);
+    expect(takeDrugHistory.map(e=>{
+      return {
+        prescriptionId: e.drugPrescriptionId,
+        userId: e.userId
+      }
+    })).toContainEqual({
+      prescriptionId: firstPrescriptionId,
+      userId: userId
+    });
+  })
+});
